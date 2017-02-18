b096790b18c671e074e0d2fd5ae8f120fff538b9
DevYmA <ymamarathunga@gmail.com>
2017-02-18 15:49:24 +0530

    rollback hello controller

--- src/main/java/com/yma/controller/HelloController.java
+++ src/main/java/com/yma/controller/HelloController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     
-    @RequestMapping("/")
+    @RequestMapping("sayhello")
     public String sayHello(){
         return "index";
     }
