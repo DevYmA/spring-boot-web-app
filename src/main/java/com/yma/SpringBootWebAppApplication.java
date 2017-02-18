030cc26988d90c9b4ec6e23528de9bafceb41a94
DevYmA <ymamarathunga@gmail.com>
2017-02-18 14:55:18 +0530

    First commit with configuration

---
+++ src/main/java/com/yma/SpringBootWebAppApplication.java
@@ -0,0 +1,19 @@
+package com.yma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootWebAppApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppApplication.class, args);
+	}
+        
+        @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebAppApplication.class);
+	}
+}
